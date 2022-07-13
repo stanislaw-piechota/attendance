8ff81890f0ed730656df7aa2f65cc0c15ac6942f
Stanislaw <s.piecho09@gmail.com>
2022-07-13 23:53:06 +0200

    db_update

--- main/models.py
+++ main/models.py
@@ -3,6 +3,21 @@ from django.db import models
 
 class VerificationCode(models.Model):
     code = models.CharField(max_length=10)
+    user_id = models.BigIntegerField()
 
     def __str__(self):
         return self.code
+
+
+class Studnet(models.Model):
+    id = models.BigIntegerField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')
+    name = models.CharField(max_length=20)
+    second_name = models.CharField(max_length=50)
+    room = models.IntegerField(default=0)
+    row = models.IntegerField(default=0)
+    col = models.IntegerField(default=0)
+    photo = models.ImageField(upload_to='images/', default=None)
+
+
+class Teacher(models.Model):
+    pass
--- main/views.py
+++ main/views.py
@@ -2,4 +2,13 @@ from django.shortcuts import render
 
 
 def index(response):
-    return render(response, "main/home.html", {})
+    # user verified
+    if response.COOKIES.get('attendance_verified'):
+        return render(response, "main/home.html", {})
+
+    # not verified, but sent code
+    if response.method == "POST":
+        if response.POST.get('code'):
+            pass
+
+    return render(response, "main/verify.html", {})
