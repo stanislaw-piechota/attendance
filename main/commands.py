8e1722ffed29b8299ec90fd0e878e97dc57716d1
Stanislaw <s.piecho09@gmail.com>
2022-09-13 08:26:11 +0200

    sending methods

--- main/commands.py
+++ main/commands.py
@@ -1,10 +1,14 @@
-import encodings
+from base64 import encode
 import qrcode as qr
 import os
 from random import choice
 from datetime import datetime as dt
 import json
 from shutil import rmtree
+from unidecode import unidecode
+from secured_files.password import *
+import smtplib, ssl
+from email.message import EmailMessage
 
 def draw_room(model, room):
     seats = model.objects.filter(room=room)
@@ -155,3 +159,42 @@ def delete_raports():
         f_date = dt.strptime(folder, '%d-%m-%Y')
         if (date-f_date).days > 30:
             rmtree(f'./main/reports/{folder}')
+
+def send_mails(st, codes):
+    with open('secured_files/to_send.txt', encoding='utf-8') as file:
+        context = ssl.create_default_context()
+
+        with smtplib.SMTP_SSL(address, port, context=context) as server:
+            server.login(mail, password)
+            for line in file.readlines():
+                data = line.split(',')
+                receive_mail = f'u{data[2]}{data[3].lower()}{unidecode(data[0][0:3]).capitalize()}\
+{unidecode(data[1][:3]).capitalize()}@liceum.p.lodz.pl'
+                name, second_name, class_name = data[0], data[1], f"{22+1-int(data[2])}{data[3].upper()}"
+                print(receive_mail, name, second_name, class_name)
+                id = st.objects.filter(name=name, second_name=second_name, class_name=class_name)[0].id
+                code = codes.objects.get(user_id=str(id))
+                message = EmailMessage()
+                message['From'] = mail
+                message['To'] = receive_mail
+                subject = "System obecności PLOPŁ"
+                text = f"""
+Kod weryfikacyjny dla ucznia: {name} {second_name}\n
+{code}
+
+Wygenerowane automatycznie, {dt.now().strftime('%H:%M:%S %d.%m.%Y')}.\n
+Nie odpowiadaj na tą wiadomość.
+"""
+                message['Subject'] = subject
+                message.set_content(text)
+                server.sendmail(mail, receive_mail, message.as_string())
+                with open('secured_files/students.txt', 'a', encoding='utf-8') as file2:
+                    file2.write(f'{line}\n')
+    with open('secured_files/to_send.txt', 'w') as file:
+        print('done')
+
+def create_db_users(st):
+    with open('secured_files/to_send.txt', encoding='utf-8') as file:
+        for line in file.readlines():
+            data = line.split(',')
+            st.objects.create(name=data[0], second_name=data[1], class_name=f"{23-int(data[2])}{data[3].capitalize()}")
