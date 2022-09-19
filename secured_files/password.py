701a75c8b265e1581092dbb181a6bf83b32fb453
Stanislaw <s.piecho09@gmail.com>
2022-09-19 15:24:55 +0200

    database fix

--- secured_files/password.py
+++
@@ -1,5 +0,0 @@
-password = 'plyxllgljfveloii'
-mail = 'plopl.system@gmail.com'
-port = 465
-address = 'smtp.gmail.com'
-pass2 = 'deyxfslcdlnkpxuf'
--- secured_files/send.py
+++
@@ -1,5 +0,0 @@
-from main.commands import send_mails as sm
-from main.models import Student as st, VerificationCode as vc
-
-
-sm(st, vc)
